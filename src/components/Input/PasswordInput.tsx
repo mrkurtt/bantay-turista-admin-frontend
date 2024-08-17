3152f6c473cdd88cd454def52694b8df05f363ee
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-08-18 00:27:56 +0800

    feat: create reusable password input

---
+++ src/components/Input/PasswordInput.tsx
@@ -0,0 +1,60 @@
+import { Input } from '@nextui-org/react';
+import React, { useState } from 'react';
+import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
+
+interface PasswordInputProps {
+	label?: string;
+	placeholder?: string;
+	variant?: 'bordered' | 'filled' | 'underlined';
+	value?: string; // Optional value to support controlled components
+	onChange?: (value: string) => void; // Callback for handling changes
+}
+
+const PasswordInput: React.FC<PasswordInputProps> = ({
+	label = 'Password',
+	placeholder = 'Enter your password',
+	variant = 'bordered',
+	value,
+	onChange,
+}) => {
+	const [isVisible, setIsVisible] = useState<boolean>(false);
+	const [password, setPassword] = useState<string>(value || ''); // If value is passed, use it; otherwise, use internal state
+
+	const toggleVisibility = () => setIsVisible(!isVisible);
+
+	const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newPassword = e.target.value;
+		setPassword(newPassword);
+
+		if (onChange) {
+			onChange(newPassword);
+		}
+	};
+
+	return (
+		<Input
+			label={label}
+			placeholder={placeholder}
+			variant={'bordered'}
+			endContent={
+				<button
+					className="focus:outline-none"
+					type="button"
+					onClick={toggleVisibility}
+					aria-label="toggle password visibility"
+				>
+					{isVisible ? (
+						<IoMdEye className="text-primary" size={25} />
+					) : (
+						<IoMdEyeOff className="text-primary" size={25} />
+					)}
+				</button>
+			}
+			type={isVisible ? 'text' : 'password'}
+			value={password}
+			onChange={handlePasswordChange}
+		/>
+	);
+};
+
+export default PasswordInput;
