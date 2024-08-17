27be09cd94016e79becfdf9420b1c5f19bb7e564
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-08-18 01:17:41 +0800

    feat: custom reusable modal

---
+++ src/components/Modal/CustomModal.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+	Modal,
+	ModalContent,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	Button,
+} from '@nextui-org/react';
+
+interface IModalProps {
+	title: string;
+	body: React.ReactNode;
+	footer?: React.ReactNode;
+	isOpen: boolean;
+	onClose: () => void;
+}
+
+const CustomModal = ({ title, body, isOpen, onClose, footer }: IModalProps) => {
+	return (
+		<Modal size={'lg'} isOpen={isOpen} onClose={onClose}>
+			<ModalContent>
+				{(onClose) => (
+					<>
+						<ModalHeader className="flex flex-col gap-1">{title}</ModalHeader>
+						<ModalBody>{body}</ModalBody>
+						<ModalFooter>{footer}</ModalFooter>
+					</>
+				)}
+			</ModalContent>
+		</Modal>
+	);
+};
+
+export default CustomModal;
