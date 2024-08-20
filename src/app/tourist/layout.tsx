3568154683329dec1b96ce99459a53edad9596fc
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-08-20 13:57:01 +0800

    refactor: tourist page metadata

--- src/app/tourist/layout.tsx
+++ src/app/tourist/layout.tsx
@@ -3,11 +3,7 @@ import { Metadata } from 'next';
 import React from 'react';
 
 export const metadata: Metadata = {
-	title: {
-		template: '%s | Bantay Turista',
-		default: 'Bantay Turista',
-	},
-	description: 'A Tourist Tracking System for Camiguin Island.',
+	title: 'Tourist',
 };
 
 export default function RootLayout({
