315a12008dc760150aa124d262e87292b7daa99c
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-08-23 19:11:52 +0800

    feat(ui): complaints

--- src/app/tourist/complaints/page.tsx
+++ src/app/tourist/complaints/page.tsx
@@ -1,7 +1,25 @@
+import GradientBtn from '@/components/Button/GradientBtn';
+import FormContainer from '@/components/Container/FormContainer';
+import Container from '@/components/Container/LayoutContainer';
+import PageTitle from '@/components/Text/PageTitle';
+import { Textarea } from '@nextui-org/react';
 import React from 'react';
 
 const Complaints = () => {
-	return <div>Complaints</div>;
+	return (
+		<Container>
+			<FormContainer>
+				<PageTitle title="Complaint" />
+				<div className="flex flex-col gap-y-6">
+					<Textarea
+						label="Complaint Description"
+						placeholder="State your complaint."
+					/>
+					<GradientBtn fullWidth label="Submit Complaint" />
+				</div>
+			</FormContainer>
+		</Container>
+	);
 };
 
 export default Complaints;
