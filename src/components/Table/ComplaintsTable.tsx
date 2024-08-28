c5a5ea363fa2839935e97e794e0d76c7f1403a57
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-08-28 18:00:27 +0800

    chore: removed unnecessary imports

--- src/components/Table/ComplaintsTable.tsx
+++ src/components/Table/ComplaintsTable.tsx
@@ -1,58 +1,8 @@
 'use client';
+
 import React, { useEffect } from 'react';
-import {
-	Table,
-	TableHeader,
-	TableColumn,
-	TableBody,
-	TableRow,
-	TableCell,
-	getKeyValue,
-} from '@nextui-org/react';
 import BreadCrumbs from '../BreadCrumbs/BreadCrumbs';
 import { useComplaintStore } from '@/stores/useComplaintStore';
-
-const rows = [
-	{
-		key: '1',
-		name: 'Tony Reichert',
-		role: 'CEO',
-		status: 'Active',
-	},
-	{
-		key: '2',
-		name: 'Zoey Lang',
-		role: 'Technical Lead',
-		status: 'Paused',
-	},
-	{
-		key: '3',
-		name: 'Jane Fisher',
-		role: 'Senior Developer',
-		status: 'Active',
-	},
-	{
-		key: '4',
-		name: 'William Howard',
-		role: 'Community Manager',
-		status: 'Vacation',
-	},
-];
-
-const columns = [
-	{
-		key: 'name',
-		label: 'NAME',
-	},
-	{
-		key: 'role',
-		label: 'ROLE',
-	},
-	{
-		key: 'status',
-		label: 'STATUS',
-	},
-];
 
 const ComplaintsTable = () => {
 	const { getAllComplaints, complaints } = useComplaintStore();
